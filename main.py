eaac253ba70422fa77ffd3c9f59a0990717cb972
DillonThornberry <54234906+DillonThornberry@users.noreply.github.com>
2023-01-05 08:37:36 -0700

    experimentation with windows for preventing mouse movement on host

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ import keyboard as kb
 from pynput import mouse
 import asyncio
 import websockets as ws
+import pyglet
 
 ACTIVATE_HOTKEY = 'alt+x'
 PORT = 8080
 
-isActive = False
-kb.add_hotkey(ACTIVATE_HOTKEY, lambda: flipActive())
-mouseListener = None
-
-
-async def main():
-    async with ws.serve(None, "", PORT):
-        await asyncio.Future()
+isActive = False   # Boolean for whether to actively listen for mkb inputs
+kb.add_hotkey(ACTIVATE_HOTKEY, lambda: flipActive())  # Listens for hotkey to turn listeners on or off
+mouseListener = None  # Event listener to mouse movements
+controller = mouse.Controller()  # Controller to move mouse
+lastPosition = np.array(controller.position)  # Stores last location of mouse on screen
+window = None
+window = pyglet.window.Window(caption='Controlling other PC')
 
 def startListeners():
     global mouseListener
+    global window
+
+    # window = pyglet.window.Window(caption='Controlling other PC') # fullscreen=True, style=pyglet.window.Window.WINDOW_STYLE_TRANSPARENT,
+
+    # Start event listener for mouse actions
     mouseListener = mouse.Listener(
-        on_move=onMove
+        on_move=onMove,
+        on_scroll=onScroll,
+        on_click=onClick
     )
     mouseListener.start()
+
 
 def killListeners():
     global mouseListener
@@ -36,18 +44,29 @@ def flipActive():
     else:
         killListeners()
 
-controller = mouse.Controller()
-lastPosition = np.array(controller.position)
+
+
+
 def onMove(x, y):
     global lastPosition
     newPosition = np.array((x, y))
     deltaPosition = newPosition - lastPosition
-    print(deltaPosition)
+    #print(deltaPosition)
     lastPosition = newPosition
 
 
+def onScroll(x, y, dx, dy):
+    print(dx, dy)
+    #controller.scroll(-dx, -dy)
 
 
+def onClick(x, y, button, pressed):
+    return
+
+
+async def main():
+    async with ws.serve(None, "", PORT):
+        await asyncio.Future()
 
 
 if __name__ == '__main__':
